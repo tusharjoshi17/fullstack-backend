3372aed64a9d8ddf75e33904705c017cbb06548d
Tushar Joshi <tusharjoshi52@gmail.com>
2023-09-01 21:08:48 +0530

    initial commit for UserController

--- src/main/java/com/tusharjoshi/fullstackbackend/controller/UserController.java
+++ src/main/java/com/tusharjoshi/fullstackbackend/controller/UserController.java
@@ -1,4 +1,18 @@
 package com.tusharjoshi.fullstackbackend.controller;
+import com.tusharjoshi.fullstackbackend.model.User;
+import com.tusharjoshi.fullstackbackend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/users")
+    User newUser(@RequestBody User newUser){
+        return userRepository.save(newUser);
+    }
 }
