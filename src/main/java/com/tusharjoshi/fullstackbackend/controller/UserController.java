127e0159eefa0124a8a3fd7e42ff257b46faf334
Tushar Joshi <tusharjoshi52@gmail.com>
2023-09-03 01:41:37 +0530

    CrossOrigin annotation for connecting back to frontend

--- src/main/java/com/tusharjoshi/fullstackbackend/controller/UserController.java
+++ src/main/java/com/tusharjoshi/fullstackbackend/controller/UserController.java
@@ -2,14 +2,12 @@ package com.tusharjoshi.fullstackbackend.controller;
 import com.tusharjoshi.fullstackbackend.model.User;
 import com.tusharjoshi.fullstackbackend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin("http://localhost:3000/")
 public class UserController {
     @Autowired
     private UserRepository userRepository;
