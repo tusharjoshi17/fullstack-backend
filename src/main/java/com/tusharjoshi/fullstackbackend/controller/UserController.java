9d596cabc3579954dd371c269513c12d26f76c68
Tushar Joshi <tusharjoshi52@gmail.com>
2023-09-02 18:02:32 +0530

    Commit for GetMapping in UserController

--- src/main/java/com/tusharjoshi/fullstackbackend/controller/UserController.java
+++ src/main/java/com/tusharjoshi/fullstackbackend/controller/UserController.java
@@ -2,17 +2,25 @@ package com.tusharjoshi.fullstackbackend.controller;
 import com.tusharjoshi.fullstackbackend.model.User;
 import com.tusharjoshi.fullstackbackend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class UserController {
     @Autowired
     private UserRepository userRepository;
 
-    @PostMapping("/users")
+    @PostMapping("/user")
     User newUser(@RequestBody User newUser){
         return userRepository.save(newUser);
+    }
+
+    @GetMapping("/user")
+    List<User> getAllUsers(){
+        return userRepository.findAll();
     }
 }
