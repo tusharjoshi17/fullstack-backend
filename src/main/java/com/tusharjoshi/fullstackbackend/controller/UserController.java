88e1185324e156ba8e762e93168bf474c2f30f94
Tushar Joshi <tusharjoshi52@gmail.com>
2023-09-12 14:55:33 +0530

    added edit option in the backend for database

--- src/main/java/com/tusharjoshi/fullstackbackend/controller/UserController.java
+++ src/main/java/com/tusharjoshi/fullstackbackend/controller/UserController.java
@@ -25,6 +25,17 @@ public class UserController {
 
     @GetMapping("/user/{id}")
     User getUserById(@PathVariable Long id){
-        return userRepository.findById(id).orElseThrow(() -> new UserNotFoundException(id));
-    }
+        return userRepository.findById(id).orElseThrow(() -> new UserNotFoundException(id)); }
+
+        @PutMapping("/user/{id}")
+                User updateUser(@RequestBody User newUser, @PathVariable Long id){
+            return userRepository.findById(id)
+                    .map(user -> {
+                        user.setName(newUser.getName());
+                        user.setUsername(newUser.getUsername());
+                        user.setEmail(newUser.getEmail());
+                        return userRepository.save(user);
+                    }).orElseThrow(()-> new UserNotFoundException(id));
+        }
+
 }
