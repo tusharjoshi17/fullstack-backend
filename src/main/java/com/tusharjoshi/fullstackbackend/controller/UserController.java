c90300609fdd4f4e67e30eef1efc02abe4607b85
Tushar Joshi <tusharjoshi52@gmail.com>
2023-09-12 19:49:59 +0530

    added deleted option in backend

--- src/main/java/com/tusharjoshi/fullstackbackend/controller/UserController.java
+++ src/main/java/com/tusharjoshi/fullstackbackend/controller/UserController.java
@@ -38,4 +38,13 @@ public class UserController {
                     }).orElseThrow(()-> new UserNotFoundException(id));
         }
 
+        @DeleteMapping("/user/{id}")
+        String deleteUser(@PathVariable Long id){
+        if(!userRepository.existsById(id)){
+            throw new UserNotFoundException(id);
+        }
+            userRepository.deleteById(id);
+        return "User deleted with id: "+id;
+        }
+
 }
